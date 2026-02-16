16561de17ca52728d103d6240511cc193b45ed95
makeshmakesh <makesh@lendsmart.ai>
2026-02-16 21:35:51 +0530

    static issue

--- maedix_q/settings.py
+++ maedix_q/settings.py
@@ -151,7 +151,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
